b264332f22e2c3b3dd5c32eccb34328f6ca757e2
Mateus Borges Rodrigues <mateusborges090@gmail.com>
2024-10-26 08:52:09 -0400

    Initial Commit

---
+++ FirstAppTests/FirstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppTests.swift
+//  FirstAppTests
+//
+//  Created by user264011 on 10/26/24.
+//
+
+import Testing
+@testable import FirstApp
+
+struct FirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
